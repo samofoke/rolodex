0b4c91f12bb10fe7d7a01bc928bc27919edf1bc6
samofoke <sabataernestmofokeng@gmail.com>
2023-02-02 14:51:39 +0200

    added basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchMonstersFields: '',
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+
+    console.log("componentDidMount");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => 
+        this.setState(
+          () => {
+            return {monsters: users};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  onSearchChange = (event) => {
+
+    const searchMonstersFields = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchMonstersFields };
+    });
+  };
+
+  render() {
+    console.log("render");
+
+    const { monsters, searchMonstersFields } = this.state;
+    const { onSearchChange} = this;
+
+    const filterMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchMonstersFields);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={onSearchChange}
+        />
+        {filterMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
